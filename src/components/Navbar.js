dd234578bc9a0cafc216ededef76af4a0419c7de
Muhammad Usman <usmansyedislamian@gmail.com>
2026-02-04 10:51:05 +0500

    Usman Logo Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="nav-logo">Portfolio</Link>
+        <Link to="/" className="nav-logo">
+          <img src="/logo.jpg" alt="Logo" className="logo" />
+        </Link>
+
         <ul className="nav-menu">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </div>
     </nav>
